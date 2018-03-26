5f8eee39ff8f6972d578dc9b4a79cce27b36b085
Makoto Tanabe <xekid78@outlook.com>
2018-03-26 11:41:40 +0900

    Add files via upload

---
+++ omikuji/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace omikuji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new System.Random();
+            var omikuji = rand.Next(1, 6);
+
+            if (omikuji == 1)
+            {
+                Console.WriteLine("大吉");
+            }
+            else if (omikuji == 2)
+            {
+                Console.WriteLine("中吉");
+            }
+            else if (omikuji == 3)
+            {
+                Console.WriteLine("小吉");
+            }
+            else if (omikuji == 4)
+            {
+                Console.WriteLine("吉");
+            }
+            else if (omikuji == 5)
+            {
+                Console.WriteLine("凶");
+            }
+            else
+            {
+                Console.WriteLine("大凶");
+            }
+
+            // 実行結果確認のための処理 ここから
+            Console.WriteLine();
+            Console.WriteLine("何かキーを押してください");
+            Console.ReadKey();
+            // ここまで
+        }
+    }
+}
